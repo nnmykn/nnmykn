cd8babe8d98de998ceb001133fb8fbd6819088dc
kan ninomiya <ninomiya@kan.run>
2025-02-18 21:20:36 +0900

    add luma room

--- kan.run/src/app/fp16-individual/page.tsx
+++ kan.run/src/app/fp16-individual/page.tsx
@@ -13,6 +13,24 @@ export default function Fp16IndividualPage() {
           全員仕事が好きで、夜中まで働いちゃいますが、入る人は気にせず帰ってください。(気にせず帰れる人が向いています)
         </p>
       </div>
+      <div className="m-4">
+        <h2 className="text-lg">FP16* Office。</h2>
+      </div>
+      <iframe
+        src="https://lumalabs.ai/embed/e5bf4020-6161-4655-aadc-80c8830f3a76?mode=sparkles&background=%23ffffff&color=%23000000&showTitle=true&loadBg=true&logoPosition=bottom-left&infoPosition=bottom-right&cinematicVideo=undefined&showMenu=false"
+        className={'w-full h-96'}
+        frameBorder="0"
+        title="luma embed"
+      />
+      <div className="m-4">
+        <h2 className="text-lg">FP16* Break room。</h2>
+      </div>
+      <iframe
+        src="https://lumalabs.ai/embed/4a907500-5c39-42cd-a011-a09a0da7d27b?mode=sparkles&background=%23ffffff&color=%23000000&showTitle=true&loadBg=true&logoPosition=bottom-left&infoPosition=bottom-right&cinematicVideo=undefined&showMenu=false"
+        className={'w-full h-96'}
+        frameBorder="0"
+        title="luma embed"
+      />
     </Page>
   )
 }
