1a13462edebd14fda5f3fa384a02c8c1b9ae1741
nnmykn <ninomiya@kan.run>
2025-03-22 22:49:14 +0900

    dynamic import

--- kan.run/src/client/components/markdown-editor.tsx
+++ kan.run/src/client/components/markdown-editor.tsx
@@ -3,7 +3,12 @@
 import { useEffect, useState } from 'react'
 import '@uiw/react-md-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
-import MDEditor from '@uiw/react-md-editor'
+import dynamic from 'next/dynamic'
+
+const MDEditor = dynamic(
+  () => import('@uiw/react-md-editor').then((mod) => mod.default),
+  { ssr: false }
+)
 
 interface MarkdownEditorProps {
   content: string
